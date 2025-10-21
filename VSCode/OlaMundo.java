ad32f4f82119c0e6a84da16a9f3de4f2907a2ce3
Weslley Rocha <weslley.nx@gmail.com>
2025-10-21 14:14:56 -0300

    Add projeto - Olá Mundo

---
+++ VSCode/OlaMundo.java
@@ -0,0 +1,14 @@
+public class OlaMundo {
+    public static void main(String[] args){
+        exibir();
+        exibir1();
+    }
+
+    public static void exibir(){
+        System.out.println ("Olá Mundo!");
+    }
+
+    public static void exibir1(){
+        System.out.println ("Como dizia o professor Gustavo Guanabara, Me livrei da maldição!");
+    }
+}

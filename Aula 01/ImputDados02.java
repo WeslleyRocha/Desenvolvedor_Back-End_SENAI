2cfc723af07e1061373d5279247a15af250f1857
Weslley Rocha <weslley.nx@gmail.com>
2025-10-22 13:45:17 -0300

    Updated Libs

--- Aula 01/ImputDados02.java
+++ Aula 01/ImputDados02.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ImputDados02{
 
 public static void main (String[] args){
@@ -6,14 +8,13 @@ int numero1;
 int numero2;
 int total;
 
-Scanner ler = new scanner(System.in);
+Scanner ler = new Scanner(System.in);
 System.out.println("Digite o primeiro valor: ");
 numero1 = ler.nextInt(); 
 
 
 System.out.println("Digite o segundo valor: ");
 numero2 = ler.nextInt();
-
 
 total = numero1 + numero2;
 System.out.println("O valor da sua soma é: " + total);

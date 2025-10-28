7ac2c09630c6f6cb59acefb498aa11e5bbc23886
Weslley Rocha <weslley.nx@gmail.com>
2025-10-28 16:54:53 -0300

    Fix - Commit

---
+++ Aula 01/Tabela_Verdade.java
@@ -0,0 +1,14 @@
+public class Tabela_Verdade {
+    public static void main(String[] args) {
+
+        int a = 0;
+        int b = 0;
+        int c = 0;
+
+        boolean retorno;
+
+
+        retorno = a > b || c < d;
+        System.out.println(retorno);
+    }
+}
